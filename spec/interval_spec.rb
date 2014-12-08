fd5d68eaf5f13792863c9a71b97540d33b6467a6
Ryan Linton <ryanl@clearsightstudio.com>
2014-12-07 16:04:31 -0800

    Add interval support

---
+++ spec/interval_spec.rb
@@ -0,0 +1,16 @@
+describe "Medic::Interval" do
+
+  before do
+    @subject = Object.new
+    @subject.extend(Medic::Interval)
+  end
+
+  describe "#interval" do
+    it "returns the correct NSDateComponents object for symbol" do
+      @subject.interval(:nine_hundred_ninety_nine_days).should.be.kind_of? NSDateComponents
+      @subject.interval(:nine_hundred_ninety_nine_days).day.should == 999
+      @subject.interval("365 days").day.should == 365
+      @subject.interval(:day).day.should == 1
+    end
+  end
+end
