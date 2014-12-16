ba5a58dff180654928070c110cea9582ff69350c
Ryan Linton <ryanl@clearsightstudio.com>
2014-12-16 09:13:18 -0800

    Add sources finder

--- app/controllers/observe_finder_controller.rb
+++ app/controllers/observe_finder_controller.rb
@@ -9,23 +9,12 @@ class ObserveFinderController < BaseController
 
   def viewDidAppear(animated)
     Medic.observe :step_count do |completion, error|
-      get_step_sources
-    end
-  end
-
-  def get_step_sources
-    query = Medic::SourceQuery.new(type: :step_count) do |query, sources, error|
-      if sources
-        sources.allObjects.each do |source|
-          NSLog source.public_methods.to_s
-          NSLog source.name.to_s
+      Medic.find_sources :step_count do |sources|
+        sources.each do |source|
+          NSLog source
         end
-      else
-        NSLog "no sources"
       end
     end
-
-    Medic.execute(query)
   end
 
 end
--- lib/medic/finders.rb
+++ lib/medic/finders.rb
@@ -8,5 +8,13 @@ module Medic
       Medic.execute(query)
     end
 
+    def find_sources(type, predicate=nil, block=Proc.new)
+      query = Medic::SourceQuery.new type: type, predicate: predicate do |query, results, error|
+        sources = results ? results.allObjects.map{ |source| source.name.to_s } : []
+        block.call(sources)
+      end
+      Medic.execute(query)
+    end
+
   end
 end
